1547a07bd0924fdada6e613a9b56c0205e51ff48
zeejfps <zeejfps@gmail.com>
2026-04-26 17:23:22 -0500

    Added Test for push instructions

--- SpaceInvadersEmulator.Core.Tests/CpuTests.cs
+++ SpaceInvadersEmulator.Core.Tests/CpuTests.cs
@@ -510,4 +510,64 @@ public class CpuTests
         Assert.Equal(10, cycles);
         Assert.Equal(expectedState, CpuState.FromCpu(cpu));
     }
+
+    [Theory]
+    [InlineData(0xC5, Reg.B)]
+    [InlineData(0xD5, Reg.D)]
+    [InlineData(0xE5, Reg.H)]
+    public void TestPushRp(byte opcode, Reg src)
+    {
+        ushort stackAddr = 0x2002;
+        var initialState = new CpuState
+        {
+            Pc = 0x10,
+            Sp = stackAddr,
+            Flags = AllFlags
+        };
+        initialState.WriteRegPair(src, 0x2030);
+
+        var mmu = new Mmu();
+        mmu.Write(initialState.Pc, opcode);
+
+        var cpu = CreateCpu(mmu, initialState);
+        var cycles = cpu.Step();
+
+        var expectedState = initialState;
+        expectedState.IncrementPcBy(1);
+        expectedState.Sp = (ushort)(stackAddr - 2);
+
+        Assert.Equal(11, cycles);
+        Assert.Equal(expectedState, CpuState.FromCpu(cpu));
+        Assert.Equal(0x30, mmu.Read((ushort)(stackAddr - 2)));
+        Assert.Equal(0x20, mmu.Read((ushort)(stackAddr - 1)));
+    }
+
+    [Fact]
+    public void TestPushPsw()
+    {
+        byte opcode = 0xF5;
+        ushort stackAddr = 0x2002;
+        var initialState = new CpuState
+        {
+            Pc = 0x10,
+            Sp = stackAddr,
+            Ra = 0xAB,
+            Flags = CpuFlags.Z
+        };
+
+        var mmu = new Mmu();
+        mmu.Write(initialState.Pc, opcode);
+
+        var cpu = CreateCpu(mmu, initialState);
+        var cycles = cpu.Step();
+
+        var expectedState = initialState;
+        expectedState.IncrementPcBy(1);
+        expectedState.Sp = (ushort)(stackAddr - 2);
+
+        Assert.Equal(11, cycles);
+        Assert.Equal(expectedState, CpuState.FromCpu(cpu));
+        Assert.Equal((byte)CpuFlags.Z, mmu.Read((ushort)(stackAddr - 2)));
+        Assert.Equal(0xAB, mmu.Read((ushort)(stackAddr - 1)));
+    }
 }
