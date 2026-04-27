7833dc045ad767b6034d3e08e6829c67746695f1
zeejfps <zeejfps@gmail.com>
2026-04-27 09:34:46 -0500

    potentially hooked up the port reading and writting

--- SpaceInvadersEmulator.Core/Machine.cs
+++ SpaceInvadersEmulator.Core/Machine.cs
@@ -1,12 +1,17 @@
+using System.Runtime.CompilerServices;
 using SpaceInvadersEmulator.Core.Intel8080;
 
 namespace SpaceInvadersEmulator.Core;
 
-public sealed class Machine
+public sealed class Machine : ICpuIO
 {
     private readonly Mmu _mmu;
     private readonly Cpu _cpu;
 
+    private byte _shiftRegHi;
+    private byte _shiftRegLo;
+    private byte _shiftRegOffset;
+    
     private int _cycleCount;
     
     public void Start()
@@ -28,5 +33,47 @@ public sealed class Machine
     private void OnTick()
     {
         _cycleCount += _cpu.Step();
+    }
+
+    public void WritePort(byte port, byte value)
+    {
+        switch (port)
+        {
+            case 2:
+                WritePort2(value);
+                break;
+            case 4:
+                WritePort4(value);
+                break;
+        }
+    }
+
+    public byte ReadPort(byte port)
+    {
+        return port switch
+        {
+            3 => ReadPort3(),
+            _ => 0
+        };
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void WritePort4(byte value)
+    {
+        _shiftRegLo = _shiftRegHi;
+        _shiftRegHi = value;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void WritePort2(byte value)
+    {
+        _shiftRegOffset = (byte)(value * 0x07);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private byte ReadPort3()
+    {
+        var value = (_shiftRegHi << 8) | _shiftRegLo;
+        return (byte)((value >> (8 - _shiftRegOffset)) & 0xFF);
     }
 }
